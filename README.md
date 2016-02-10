c4194758d0d9cda9dd26d8c4428e971ea67e1bfe
summea <chkmyemailnow@yahoo.com>
2016-02-10 10:29:39 -0800

    updates

---
+++ README.md
@@ -0,0 +1,6 @@
+# Less-Sodium
+
+A simple WordPress plugin experiment intended to catch spam. 
+
+## License
+MIT (Please check LICENSE file for license information.)
--- less-sodium.php
+++ less-sodium.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Less Sodium
-Plugin URI: ...
+Plugin URI: https://github.com/summea/less-sodium
 Description: Simple WordPress plugin to catch some spam.
-Version: 0.0.1
+Version: 0.0.5
 Author: summea
 License: GPL2
 
@@ -23,43 +23,85 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-function check_spam() {
-  /* For now, spam checking is done by just checking
-     for common spam words in the comment meta data. */
-  $spam_list = array(
-    'ugg',
-    'uggs',
-    'nike',
-    'louisvuitton',
-    'louis vuitton',
-    'gucci',
-    'index.php',
-    '.php',
-    'nba',
-    'pharmacy',
-    'hermes',
-    'store',
-    'backlinks',
-    'xrumer',
-    'cash',
-    'survey',
-    'calculator',
-    'clothes',
-    'reddit',
-    'singles',
-    'loan'
-  );
-  foreach($spam_list as $keyword) {
-    if (preg_match("/$keyword/i", $_POST['author']) ||
-        preg_match("/$keyword/i", $_POST['email']) ||
-        preg_match("/$keyword/i", $_POST['url'])) {
-      # keyword found... probably spam
-      echo '<h1>We could be wrong, but...</h1>Sorry, your comment looks like spam.  If you are a real person, please try contacting my email address if you would like to leave a comment!  Thanks.<br /><br />Sincerely,<br />Less Sodium';
-      exit;
-    }
+define("BASE_DIR", "/less-sodium");
+define("DATA_DIR", BASE_DIR . "/data");
+
+/**
+ * Check comment form for possible spam before saving to database.
+ * @return int 
+ */
+function check_for_spam() {
+  $error_message =  '<b>We could be wrong, but...</b><br />' .
+                    'Your comment looks like spam.<br />' .
+                    'Please find another way to contact us...!' . 
+                    '<br /><br />Sincerely,<br />Less Sodium';
+
+  # check email address for clues
+  if (preg_match("/^[A-Z]/", $_POST['email'])) {
+    log_spam();
+    wp_die($error_message); # probably spam
+    exit;
   }
+
+  # look through wordlist for common spam keywords
+  if (file_exists(WP_PLUGIN_DIR . DATA_DIR . '/wordlist.txt')) {
+    $data = file_get_contents(WP_PLUGIN_DIR . DATA_DIR . '/wordlist.txt', true);
+    $spam_keywords = split("\n", $data);
+    array_pop($spam_keywords); # remove the last newline
+
+    foreach($spam_keywords as $keyword) {
+      if (preg_match("/$keyword/i", $_POST['author']) ||
+          preg_match("/$keyword/i", $_POST['email']) ||
+          preg_match("/$keyword/i", $_POST['url']) ||
+          preg_match("/$keyword/i", $_POST['comment']))
+      {
+        log_spam();
+        wp_die($error_message); # keyword found... probably spam
+        exit;
+      }
+    }
+  } else {
+    # create wordlist.txt
+    $file = fopen(WP_PLUGIN_DIR . DATA_DIR . '/wordlist.txt', 'w');
+    fclose($file);
+  }
+  return 1;
 }
 
-add_action('comment_post', 'check_spam');
+/**
+ * Log spam to text file.
+ * @return int count
+ */
+function log_spam() {
+  $count = 0;
+  if (file_exists(WP_PLUGIN_DIR . DATA_DIR . '/spamcount.txt')) {
+    $data = file_get_contents(WP_PLUGIN_DIR . DATA_DIR . 
+      '/spamcount.txt', true);
+    $filtered_data = preg_replace('/[^0-9]/', '', $data);
+    $count += (int)$filtered_data + 1;
+  }
+  $file = fopen(WP_PLUGIN_DIR . DATA_DIR . '/spamcount.txt', 'w');
+  fwrite($file, $count);
+  fclose($file);
+  return $count;
+}
+
+/**
+ * Get spam count from spamcount.txt
+ * @return int count
+ */
+function get_spam_count() {
+  $count = 0;
+  if (file_exists(WP_PLUGIN_DIR . DATA_DIR . '/spamcount.txt')) {
+    $data = file_get_contents(WP_PLUGIN_DIR . DATA_DIR . 
+      '/spamcount.txt', true);
+    $filtered_data = preg_replace('/[^0-9]/', '', $data);
+    if ((int)$filtered_data > 0)
+      $count = (int)$filtered_data;
+  }
+  return $count;
+}
+
+add_action('check_comment_flood', 'check_for_spam');
 
 ?>
